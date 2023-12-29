56afabd2213e91c0c08389f26195cee2711b4cf8
WarterPL <96202445+WarterPL@users.noreply.github.com>
2023-12-30 00:30:21 +0100

    Update-preview2.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace ShortcutFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.nameOfApp = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.adressLabel = new System.Windows.Forms.Label();
+            this.Adress = new System.Windows.Forms.TextBox();
+            this.customIMGlabel = new System.Windows.Forms.Label();
+            this.CustomIMG = new System.Windows.Forms.TextBox();
+            this.NewItem = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.run = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.RunViaLabel = new System.Windows.Forms.Label();
+            this.RunVia = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.listView1.HideSelection = false;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(372, 403);
+            this.listView1.TabIndex = 29;
+            this.listView1.TileSize = new System.Drawing.Size(255, 30);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // nameOfApp
+            // 
+            this.nameOfApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameOfApp.Location = new System.Drawing.Point(548, 36);
+            this.nameOfApp.Name = "nameOfApp";
+            this.nameOfApp.Size = new System.Drawing.Size(453, 27);
+            this.nameOfApp.TabIndex = 30;
+            this.nameOfApp.TextChanged += new System.EventHandler(this.nameOfApp_TextChanged);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nameLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nameLabel.Location = new System.Drawing.Point(390, 39);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(139, 20);
+            this.nameLabel.TabIndex = 31;
+            this.nameLabel.Text = "Name of program";
+            // 
+            // adressLabel
+            // 
+            this.adressLabel.AutoSize = true;
+            this.adressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.adressLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.adressLabel.Location = new System.Drawing.Point(421, 92);
+            this.adressLabel.Name = "adressLabel";
+            this.adressLabel.Size = new System.Drawing.Size(108, 20);
+            this.adressLabel.TabIndex = 33;
+            this.adressLabel.Text = "Adress to file";
+            this.adressLabel.Click += new System.EventHandler(this.adressLabel_Click);
+            // 
+            // Adress
+            // 
+            this.Adress.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Adress.Location = new System.Drawing.Point(548, 89);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(453, 24);
+            this.Adress.TabIndex = 32;
+            this.Adress.TextChanged += new System.EventHandler(this.Adress_TextChanged);
+            // 
+            // customIMGlabel
+            // 
+            this.customIMGlabel.AutoSize = true;
+            this.customIMGlabel.BackColor = System.Drawing.Color.Red;
+            this.customIMGlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.customIMGlabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.customIMGlabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.customIMGlabel.Location = new System.Drawing.Point(412, 156);
+            this.customIMGlabel.Name = "customIMGlabel";
+            this.customIMGlabel.Size = new System.Drawing.Size(117, 20);
+            this.customIMGlabel.TabIndex = 35;
+            this.customIMGlabel.Text = "Custom image";
+            // 
+            // CustomIMG
+            // 
+            this.CustomIMG.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CustomIMG.Location = new System.Drawing.Point(548, 153);
+            this.CustomIMG.Name = "CustomIMG";
+            this.CustomIMG.Size = new System.Drawing.Size(453, 27);
+            this.CustomIMG.TabIndex = 34;
+            this.CustomIMG.TextChanged += new System.EventHandler(this.CustomIMG_TextChanged);
+            // 
+            // NewItem
+            // 
+            this.NewItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.NewItem.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NewItem.Location = new System.Drawing.Point(709, 186);
+            this.NewItem.Name = "NewItem";
+            this.NewItem.Size = new System.Drawing.Size(143, 30);
+            this.NewItem.TabIndex = 36;
+            this.NewItem.Text = "New shortcut";
+            this.NewItem.UseVisualStyleBackColor = false;
+            this.NewItem.Click += new System.EventHandler(this.NewItem_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Delete.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Delete.Location = new System.Drawing.Point(858, 186);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(143, 30);
+            this.Delete.TabIndex = 37;
+            this.Delete.Text = "Delete shortcut";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // run
+            // 
+            this.run.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.run.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.run.Location = new System.Drawing.Point(548, 186);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(155, 30);
+            this.run.TabIndex = 38;
+            this.run.Text = "Run application";
+            this.run.UseVisualStyleBackColor = false;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "adress";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(801, 222);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 39;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RunViaLabel
+            // 
+            this.RunViaLabel.AutoSize = true;
+            this.RunViaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RunViaLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RunViaLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.RunViaLabel.Location = new System.Drawing.Point(464, 122);
+            this.RunViaLabel.Name = "RunViaLabel";
+            this.RunViaLabel.Size = new System.Drawing.Size(65, 20);
+            this.RunViaLabel.TabIndex = 41;
+            this.RunViaLabel.Text = "Run via";
+            // 
+            // RunVia
+            // 
+            this.RunVia.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RunVia.Location = new System.Drawing.Point(548, 119);
+            this.RunVia.Name = "RunVia";
+            this.RunVia.Size = new System.Drawing.Size(453, 27);
+            this.RunVia.TabIndex = 40;
+            this.RunVia.TextChanged += new System.EventHandler(this.RunVia_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1010, 430);
+            this.Controls.Add(this.RunViaLabel);
+            this.Controls.Add(this.RunVia);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.NewItem);
+            this.Controls.Add(this.customIMGlabel);
+            this.Controls.Add(this.CustomIMG);
+            this.Controls.Add(this.adressLabel);
+            this.Controls.Add(this.Adress);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameOfApp);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shortcut menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox nameOfApp;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label adressLabel;
+        private System.Windows.Forms.TextBox Adress;
+        private System.Windows.Forms.Label customIMGlabel;
+        private System.Windows.Forms.TextBox CustomIMG;
+        private System.Windows.Forms.Button NewItem;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label RunViaLabel;
+        private System.Windows.Forms.TextBox RunVia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShortcutFolder
+{
+    public partial class Form1 : Form
+    {
+        static int values;
+        static List<string> name = new List<string>();
+        static List<string> adres = new List<string>();
+        static List<string> via = new List<string>();
+        static List<string> mypngadres = new List<string>();
+        int index = 0;
+
+        string myFile = "file", path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveData()
+        {
+            StreamWriter writer = new StreamWriter(myFile);
+            values = adres.ToArray().Length;
+            writer.WriteLine(values);
+            for (int i = 0; i < values; i++)
+            {
+                writer.WriteLine($"#### SLOT {i+1} #####");
+                writer.WriteLine(name[i]);
+                writer.WriteLine(adres[i]);
+                writer.WriteLine(via[i]);
+                writer.WriteLine(mypngadres[i]);
+            }
+            writer.Close();
+        }
+
+        private void LoadData()
+        {
+            if(!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            if(!File.Exists(myFile))
+                File.WriteAllText(myFile, "0");
+
+            StreamReader reader = new StreamReader(myFile);
+            values = Convert.ToInt32(reader.ReadLine());
+            for(int i = 0; i < values; i++)
+            {
+                reader.ReadLine();
+                name.Add(reader.ReadLine());
+                adres.Add(reader.ReadLine());
+                via.Add(reader.ReadLine());
+                mypngadres.Add(reader.ReadLine());
+            }
+            reader.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = @"C:/ShortcutsData/";
+            string programname = Path.GetFileName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            myFile = path + (programname.Substring(0, programname.Length-4)) + ".sdf";
+
+            LoadData();
+
+            if (values == 0)
+                NewItem_Click(null, null);
+
+            RefreshList();
+            RefreshFields();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(index < values)
+                listView1.Items[index].ForeColor = Color.White;
+
+            index = listView1.FocusedItem.Index;
+
+            RefreshFields();
+
+            listView1.SelectedItems.Clear();
+            listView1.Items[index].ForeColor = Color.Cyan;
+        }
+
+        private void nameOfApp_TextChanged(object sender, EventArgs e)
+        {
+            if(index >= 0)
+                name[index] = nameOfApp.Text;
+            RefreshList();
+        }
+
+        private void Adress_TextChanged(object sender, EventArgs e)
+        {
+            if (index >= 0)
+                adres[index] = Adress.Text;
+            RefreshList();
+        }
+
+        private void RunVia_TextChanged(object sender, EventArgs e)
+        {
+            if (index >= 0)
+                mypngadres[index] = CustomIMG.Text;
+            RefreshList();
+        }
+
+        private void CustomIMG_TextChanged(object sender, EventArgs e)
+        {
+            if (index >= 0)
+                mypngadres[index] = CustomIMG.Text;
+            RefreshList();
+        }
+
+
+
+        private void NewItem_Click(object sender, EventArgs e)
+        {
+            name.Add("NEW SHORTCUT");
+            adres.Add("<Put path here>");
+            mypngadres.Add("-default");
+            via.Add("-systemSettings");
+
+            values++;
+
+            if (values == 1)
+                RefreshFields();
+
+            RefreshList();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (values <= 0 || index >= values)
+                return;
+
+            DialogResult result = MessageBox.Show($"Would you like to delete record?" +
+                $"\nName: {name[index]}" +
+                $"\nAdress: {adres[index]}", "Shortcut folder", MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            name.RemoveAt(index);
+            adres.RemoveAt(index);
+            via.RemoveAt(index);
+            mypngadres.RemoveAt(index);
+            values--;
+
+            RefreshList();
+            RefreshFields();
+        }
+
+
+
+        private void RefreshList()
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < values; i++)
+            {
+                ListViewItem item = new ListViewItem(name[i]);
+                listView1.Items.Add(item);
+            }
+
+            if(index >= 0 && index < values)
+            listView1.Items[index].ForeColor = Color.Cyan;
+
+
+
+            nameOfApp.Enabled = index >= values ?  false : true;
+            Adress.Enabled = index >= values ? false : true;
+            RunVia.Enabled = index >= values ? false : true;
+            CustomIMG.Enabled = index >= values ? false : true;
+
+            SaveData();
+        }
+
+        void RefreshFields()
+        {
+            if (index < 0 || index >= values)
+                return;
+
+            CustomIMG.Text = mypngadres[index];
+            nameOfApp.Text = name[index];
+            Adress.Text = adres[index];
+            RunVia.Text = via[index];
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Adress.Text = openFileDialog1.FileName;
+            adres[index] = openFileDialog1.FileName;
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = adres[index],
+                    Arguments = via[index] == "-systemSettings" ? null : via[index]
+                };
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR HANDLER", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void adressLabel_Click(object sender, EventArgs e)
+        { openFileDialog1.ShowDialog(); }
+    }
+}
